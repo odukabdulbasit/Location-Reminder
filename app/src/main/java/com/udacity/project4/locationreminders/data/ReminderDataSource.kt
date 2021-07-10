4da877a03249c3c8890c8fb06cabc5b7211a698c
Abdulbasit <2016212056@cumhuriyet.edu.tr>
2021-07-10 15:55:49 +0300

    created new function to refresh reminders and observe the live data

--- app/src/main/java/com/udacity/project4/locationreminders/data/ReminderDataSource.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/data/ReminderDataSource.kt
@@ -1,5 +1,6 @@
 package com.udacity.project4.locationreminders.data
 
+import androidx.lifecycle.LiveData
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
 
@@ -11,4 +12,8 @@ interface ReminderDataSource {
     suspend fun saveReminder(reminder: ReminderDTO)
     suspend fun getReminder(id: String): Result<ReminderDTO>
     suspend fun deleteAllReminders()
+    suspend fun refreshReminders()
+    suspend fun refreshReminder(id: String)
+    suspend fun observeReminders(): LiveData<Result<List<ReminderDTO>>>
+    suspend fun observeTask(reminderId: String): LiveData<Result<ReminderDTO>>
 }
